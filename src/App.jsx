62949473521a1de9b614a803d0b5538d5eb667ff
Ariel <arielpagajr@gmail.com>
2025-12-23 13:25:22 +0800

    second comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+
+  const name = "Ariel";
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p>magandang umaga {name}</p>
+    </>
+  )
+}
